2da489ff1702813d5fe895b06da5b8bacd48d3f6
ssaileshverma <107744315+ssaileshverma@users.noreply.github.com>
2022-06-18 13:27:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.body.addEventListener("mousemove",event => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    gsap.set(".cursor",{
+        x:mouseX,
+        y:mouseY
+    })
+    gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+        stagger: -0.1
+    });
+});
